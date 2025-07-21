57bd516b24eecd712f30a1985a12d482798d9beb
vishnukuntal <35869854+vishnukuntal@users.noreply.github.com>
2025-07-21 18:15:39 +0530

    Update main.tf

--- aws-task-1/main.tf
+++ aws-task-1/main.tf
@@ -13,6 +13,5 @@ provider "aws" {
   resource "aws_instance" "terraform" {
     instance_type = "t2.micro"
      ami = "ami-020cba7c55df1f615"
-     subnet_id = "subnet-0031964253c869a4c"
-     
+     subnet_id = "subnet-0031964253c869a4c"     
   }
