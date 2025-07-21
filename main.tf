5f726ab8e90f764a6045502d58d6ecc81618d5f8
vishnukuntal <35869854+vishnukuntal@users.noreply.github.com>
2025-07-21 18:08:45 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  }
+  resource "aws_instance" "terraform" {
+    instance_type = "t2.micro"
+     ami = "ami-020cba7c55df1f615"
+     subnet_id = "subnet-0cc3791dd1a513b6a"
+     
+  }
