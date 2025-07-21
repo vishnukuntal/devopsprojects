3efa6683f92362961483623ce8f37016237b0abc
vishnukuntal <35869854+vishnukuntal@users.noreply.github.com>
2025-07-21 19:10:10 +0530

    Create backend.tf

---
+++ aws-task-1/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-s3-upgrad" # change this
+    key            = "tf-s3-upgrad/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+    use_lockfile = false
+  }
+}
