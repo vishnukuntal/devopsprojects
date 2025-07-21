14a47e4115d3147b9dc7ebc4a5633b6fd53b2a01
vishnukuntal <35869854+vishnukuntal@users.noreply.github.com>
2025-07-21 19:09:43 +0530

    Update main.tf

--- aws-task-1/main.tf
+++ aws-task-1/main.tf
@@ -13,5 +13,22 @@ provider "aws" {
   resource "aws_instance" "terraform" {
     instance_type = "t2.micro"
      ami = "ami-020cba7c55df1f615"
-     subnet_id = "subnet-0031964253c869a4c"     
+     subnet_id = "subnet-0031964253c869a4c"
+     
   }
+
+
+  resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "tf-s3-upgrad" # change this
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
